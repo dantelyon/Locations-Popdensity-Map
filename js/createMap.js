eb14526daaeee3662cedde527c1b790dc0897d78
dantelyon <dightmisc@gmail.com>
2021-07-06 17:19:13 +0200

    adjusted map settings.

--- js/createMap.js
+++ js/createMap.js
@@ -1,16 +1,11 @@
 
 if (!mapboxgl.supported()) alert('Your browser does not support Mapbox GL');
 
-const myToken = 'pk.eyJ1IjoiZGlnaHQiLCJhIjoiY2p4ZW5nanRjMG9wMzNvczhxOXprMXl4NiJ9.fmrU8kIKNnTnb6KyJ9Y1Hw'
-const testToken = 'pk.eyJ1IjoiZGlnaHQiLCJhIjoiY2p4ZW5nanRjMG9wMzNvczhxOXprMXl4NiJ9.fmrU8kIKNnTnb6KyJ9Y1Hw'
-const myStyle = 'mapbox://styles/dight/ckj41nuc91m7j19nztxq3vzp6'
-const testStyle = 'mapbox://styles/mapbox/streets-v11'
-
-mapboxgl.accessToken = testToken;
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGlnaHQiLCJhIjoiY2p4ZW5nanRjMG9wMzNvczhxOXprMXl4NiJ9.fmrU8kIKNnTnb6KyJ9Y1Hw';
 
 const MAP_SETTINGS = {
     container: 'map',
-    style: testStyle,
+    style: 'mapbox://styles/dight/ckj41nuc91m7j19nztxq3vzp6',
     center: [14.650297690342082, 59.112382521764154],
     zoom: 3,
 }
